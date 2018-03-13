bf9740319e09fd861166894b28ebaf033f2eb452
Satch Grant <grantsrb@gmail.com>
2018-03-13 18:56:00 +0000

    change parameters

--- experiment_queue.sh
+++ experiment_queue.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-python3 train_model.py resume=True exp_name=bnormconv model_type=conv n_frame_stack=4 norm_advs=True norm_batch_advs=False use_nstep_rets=True clip_vals=True decay_eps=True use_bnorm=True epsilon=.2 n_tsteps=128 n_envs=16 n_rollouts=16 batch_size=128 n_epochs=3 env_type=Pong-v0 max_tsteps=10000000 max_norm=1. val_const=1 entropy_const=.01 
 
+python3 train_model.py resume=False exp_name=conv model_type=conv n_frame_stack=4 norm_advs=True norm_batch_advs=False use_nstep_rets=True clip_vals=True decay_eps=True decay_lr=True use_bnorm=True epsilon=.20 n_tsteps=256 n_envs=16 n_rollouts=16 batch_size=128 n_epochs=4 env_type=Pong-v0 max_tsteps=20000000 max_norm=.4 val_const=.75 entropy_const=.001 bootstrap_next=True lr=1e-3 lambda_=.95 gamma=.99 fresh_advs=False
 
-python3 train_model.py exp_name=dataconvmaxnorm model_type=conv n_frame_stack=4 norm_advs=False use_nstep_rets=True norm_batch_advs=True clip_vals=True decay_eps=True epsilon=.2 n_tsteps=16 n_envs=16 n_rollouts=120 batch_size=128 n_epochs=4 env_type=Pong-v0 max_tsteps=10000000 max_norm=0.5 val_const=1 entropy_const=.005 
-
-python3 train_model.py exp_name=dataconvtsteps model_type=conv n_frame_stack=4 norm_advs=False use_nstep_rets=True norm_batch_advs=True clip_vals=True decay_eps=True epsilon=.2 n_tsteps=32 n_envs=16 n_rollouts=120 batch_size=128 n_epochs=4 env_type=Pong-v0 max_tsteps=10000000 max_norm=0.05 val_const=1 entropy_const=.01 
+python3 train_model.py resume=True exp_name=testing model_type=dense n_frame_stack=2 norm_advs=True norm_batch_advs=False use_nstep_rets=True clip_vals=True decay_eps=True decay_lr=True use_bnorm=True epsilon=.10 n_tsteps=256 n_envs=16 n_rollouts=16 batch_size=128 n_epochs=4 env_type=Pong-v0 max_tsteps=10000000 max_norm=.4 val_const=.75 entropy_const=.001 bootstrap_next=True lr=1e-3 lambda_=.95 gamma=.99 fresh_advs=False
