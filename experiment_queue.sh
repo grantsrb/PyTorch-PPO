051265c0e1160046a805427cf892ffb0ed76c5e5
Saetch Grant <grantsrb@gmail.com>
2018-06-18 10:45:02 -0700

    change hyperparams in script

--- experiment_queue.sh
+++ experiment_queue.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf ~/.nv/
+killall python3
 
-python3 main.py exp_name=reluproj2 resume=False env_type=Pong-v0 model_type=conv n_frame_stack=3 norm_advs=True norm_batch_advs=False use_nstep_rets=False clip_vals=False decay_eps=True decay_lr=True decay_entr=True use_bnorm=False epsilon=.2 epsilon_low=.1 n_tsteps=64 n_envs=9 n_rollouts=44 batch_size=128 n_epochs=4 max_tsteps=40000000 max_norm=.1 val_const=1 entr_coef_high=.01 entr_coef_low=.0009 lr=1e-4 lr_low=1e-11 lambda_=.95 gamma=.995
+python3 main.py exp_name=bigval resume=False env_type=Breakout-v0 optim_type=adam model_type=conv n_frame_stack=3 norm_advs=False norm_batch_advs=True use_nstep_rets=False clip_vals=False decay_eps=True decay_lr=False decay_entr=False incr_gamma=False use_bnorm=False n_envs=11 n_rollouts=12 n_tsteps=256 batch_size=256 n_epochs=3 max_tsteps=20000000 max_norm=.5 val_const=.1 epsilon=.17 epsilon_low=.13 entr_coef=.01 entr_coef_low=.007 lr=.0001 lr_low=1e-5 lambda_=.95 gamma=.99 gamma_high=.995
 
