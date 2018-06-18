8826c216f8282b5b86fa59531017e3c367e09abf
Saetch Grant <grantsrb@gmail.com>
2018-06-18 10:34:57 -0700

    make algorithm a class for easier hyperparameter searches

--- main.py
+++ main.py
@@ -1,194 +1,26 @@
-import sys
-import gym
-from logger import Logger
-from runner import Runner
-from updater import Updater
-import torch
-from torch.autograd import Variable
-import numpy as np
-import gc
-import resource
+from hyperparams import HyperParams, hyper_search, make_hyper_range
+from ppo import PPO
 import torch.multiprocessing as mp
-import copy
-import time
-from hyperparameters import HyperParams
-from collections import deque
-from utils import cuda_if
 
-def find_maxmin(deq):
-    max_val, min_val = deq[0],deq[0]
-    for i in range(len(deq)):
-        if deq[i] > max_val:
-            max_val = deq[i]
-        if deq[i] < min_val:
-            min_val = deq[i]
-    return max_val, min_val
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     mp.set_start_method('forkserver')
-
-    # Set Hyperparameters
+    ppo_trainer = PPO()
     hyper_params = HyperParams()
     hyps = hyper_params.hyps
+    hyp_ranges = {
+                "gamma": [.98, .99, .995],
+                "lambda_": [.94, .95, .96, .97], 
+                "lr": make_hyper_range(5e-5,1e-3,5,"log"),
+                "val_const": [.005, .01, .1, 1],
+                "entr_coef": [7e-3, .02] 
+                }
+    keys = list(hyp_ranges.keys())
+    hyps['env_type'] = "Breakout-v0"
+    hyps['exp_name'] = "brkoutlongrolls"
+    hyps['n_tsteps'] = 256
+    hyps['n_rollouts'] = 11
+    hyps['n_envs'] = 11
+    hyps['max_tsteps'] = 3000000
 
-    # Print Hyperparameters To Screen
-    items = list(hyps.items())
-    for k, v in sorted(items):
-        print(k+":", v)
+    hyper_search(hyper_params.hyps, hyp_ranges, keys, 0, ppo_trainer)
 
-    # Make Save Files
-    net_save_file = hyps['exp_name']+"_net.p"
-    best_net_file = hyps['exp_name']+"_best.p"
-    best_by_diff_file = hyps['exp_name']+"_bestdiff.p"
-    optim_save_file = hyps['exp_name']+"_optim.p"
-    log_file = hyps['exp_name']+"_log.txt"
-    if hyps['resume']: log = open(log_file, 'a')
-    else: log = open(log_file, 'w')
-    for k, v in sorted(items):
-        log.write(k+":"+str(v)+"\n")
-
-    # Miscellaneous Variable Prep
-    logger = Logger()
-    shared_len = hyps['n_tsteps']*hyps['n_rollouts']
-    env = gym.make(hyps['env_type'])
-    obs = env.reset()
-    prepped = hyps['preprocess'](obs)
-    hyps['state_shape'] = [hyps['n_frame_stack']] + [*prepped.shape[1:]]
-    if hyps['env_type'] == "Pong-v0":
-        action_size = 3
-    else:
-        action_size = env.action_space.n*(hyps['env_type']!="Pong-v0")
-    hyps['action_shift'] = (4-action_size)*(hyps['env_type']=="Pong-v0") 
-    print("Obs Shape:,",obs.shape)
-    print("Prep Shape:,",prepped.shape)
-    print("State Shape:,",hyps['state_shape'])
-    print("Num Samples Per Update:", shared_len)
-    print("Samples Wasted in Update:", shared_len % hyps['batch_size'])
-    del env
-
-    # Prepare Shared Variables
-    shared_data = {'states': cuda_if(torch.zeros(shared_len, *hyps['state_shape']).share_memory_()),
-            'next_states': cuda_if(torch.zeros(shared_len, *hyps['state_shape']).share_memory_()),
-            'rewards': cuda_if(torch.zeros(shared_len).share_memory_()),
-            'actions': torch.zeros(shared_len).long().share_memory_(),
-            'dones': cuda_if(torch.zeros(shared_len).share_memory_())}
-    n_rollouts = hyps['n_rollouts']
-    gate_q = mp.Queue(n_rollouts)
-    stop_q = mp.Queue(n_rollouts)
-    reward_q = mp.Queue(1)
-    reward_q.put(-1)
-
-    # Make Runners
-    runners = []
-    for i in range(hyps['n_envs']):
-        runner = Runner(shared_data, hyps, gate_q, stop_q, reward_q)
-        runners.append(runner)
-
-    # Make Network
-    net = hyps['model'](hyps['state_shape'], action_size, bnorm=hyps['use_bnorm'])
-    if hyps['resume']:
-        net.load_state_dict(torch.load(net_save_file))
-    base_net = copy.deepcopy(net)
-    net = cuda_if(net)
-    net.share_memory()
-    base_net = cuda_if(base_net)
-
-    # Start Data Collection
-    print("Making New Processes")
-    procs = []
-    for i in range(len(runners)):
-        proc = mp.Process(target=runners[i].run, args=(net,))
-        procs.append(proc)
-        proc.start()
-        print(i, "/", len(runners), end='\r')
-    for i in range(n_rollouts):
-        gate_q.put(i)
-
-    # Make Updater
-    updater = Updater(base_net, hyps['lr'], entr_coef=hyps['entr_coef'], value_const=hyps['val_const'], gamma=hyps['gamma'], lambda_=hyps['lambda_'], max_norm=hyps['max_norm'], batch_size=hyps['batch_size'], n_epochs=hyps['n_epochs'], cache_size=hyps['cache_size'], epsilon=hyps['epsilon'], clip_vals=hyps['clip_vals'], norm_advs=hyps['norm_advs'], norm_batch_advs=hyps['norm_batch_advs'], eval_vals=hyps['eval_vals'], use_nstep_rets=hyps['use_nstep_rets'], optim_type=hyps['optim_type'])
-    if hyps['resume']:
-        updater.optim.load_state_dict(torch.load(optim_save_file))
-    updater.optim.zero_grad()
-    updater.net.train(mode=True)
-    updater.net.req_grads(True)
-
-    # Prepare Decay Precursors
-    entr_coef_diff = hyps['entr_coef'] - hyps['entr_coef_low']
-    epsilon_diff = hyps['epsilon'] - hyps['epsilon_low']
-    lr_diff = hyps['lr'] - hyps['lr_low']
-    gamma_diff = hyps['gamma_high'] - hyps['gamma']
-
-    # Training Loop
-    past_rews = deque([0]*hyps['n_past_rews'])
-    last_avg_rew = 0
-    best_rew_diff = 0
-    best_avg_rew = 0
-    epoch = 0
-    T = 0
-    while T < hyps['max_tsteps']:
-        basetime = time.time()
-        epoch += 1
-
-        # Collect data
-        for i in range(n_rollouts):
-            stop_q.get()
-        T += shared_len
-
-        # Calculate the Loss and Update nets
-        updater.update_model(shared_data)
-        net.load_state_dict(updater.net.state_dict()) # update all collector nets
-        
-        # Resume Data Collection
-        for i in range(n_rollouts):
-            gate_q.put(i)
-
-        # Decay HyperParameters
-        if hyps['decay_eps']:
-            updater.epsilon = (1-T/(hyps['max_tsteps']))*epsilon_diff + hyps['epsilon_low']
-        if hyps['decay_lr']:
-            new_lr = (1-T/(hyps['max_tsteps']))*lr_diff + hyps['lr_low']
-            updater.new_lr(new_lr)
-        if hyps['decay_entr']:
-            updater.entr_coef = entr_coef_diff*(1-T/(hyps['max_tsteps']))+hyps['entr_coef_low']
-        if hyps['incr_gamma']:
-            updater.gamma = gamma_diff*(T/(hyps['max_tsteps']))+hyps['gamma']
-
-        # Reward Stats
-        avg_reward = reward_q.get()
-        reward_q.put(avg_reward)
-        past_rews.popleft()
-        past_rews.append(avg_reward)
-        max_rew, min_rew = find_maxmin(past_rews)
-        avg_rew_diff = avg_reward - last_avg_rew
-        last_avg_rew = avg_reward
-
-        # Save Model
-        if avg_rew_diff > best_rew_diff:
-            best_rew_diff = avg_rew_diff
-            updater.save_model(best_by_diff_file, None)
-        if avg_reward > best_avg_rew:
-            best_avg_rew = avg_reward
-            updater.save_model(best_net_file, None)
-        if epoch % 10 == 0:
-            updater.save_model(net_save_file, optim_save_file)
-
-        # Print Epoch Data
-        updater.print_statistics()
-        avg_action = shared_data['actions'].float().mean().item()
-        print("Epoch", epoch, "– T =", T)
-        print("Grad Norm:", float(updater.norm), "– Avg Action:", avg_action, "– Best AvgRew:", best_avg_rew, "– Best Diff:", best_rew_diff)
-        print("Avg Rew:", avg_reward, "– High:", max_rew, "– Low:", min_rew, end='\n')
-        updater.log_statistics(log, T, avg_reward, avg_action)
-        updater.info['AvgRew'] = avg_reward
-        logger.append(updater.info, x_val=T)
-
-        # Check for memory leaks
-        gc.collect()
-        max_mem_used = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-        print("Time:", time.time()-basetime)
-        print("Memory Used: {:.2f} memory\n".format(max_mem_used / 1024))
-
-    logger.make_plots(hyps['exp_name'])
-    # Close processes
-    for p in procs:
-        p.terminate()
